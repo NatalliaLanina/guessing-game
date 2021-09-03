e14a42d75e779209210ea1dbf6e7ce17a3e56a45
Natallia Lanina <natalielanina@yahoo.com>
2021-09-03 12:36:22 +0300

    docs: gassing game code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.value = Math.round((this.min + this.max) / 2);
+        return this.value;
     }
 
     lower() {
-
+        this.max = this.value;
     }
 
     greater() {
-
+        this.min = this.value;
     }
-}
-
+}    
 module.exports = GuessingGame;
